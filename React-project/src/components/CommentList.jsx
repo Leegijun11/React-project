b65b61c00fdbc20f0ae689cebf12f93ad8788008
deokhyun <enosh98@naver.com>
2026-03-16 20:10:50 +0900

    0316:20:10

--- React-project/src/components/CommentList.jsx
+++ React-project/src/components/CommentList.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { useComments } from '../context/CommentProvider';
 
-const CommentList = () => {
+const CommentList = ({postId,userId}) => {    
+    const { comments, deleteComent} =useComments();
+
+    const postComments = comments.filter((c)=> c.postId === postId);
+
     return (
-        <div>
-            
-        </div>
+        <ul>
+            {postComments.length === 0 && <p>댓글이 없습니다</p>}
+            {postComments.map((c)=>(
+                <li key={c.id}>{c.userId} : {c.commentText}
+                <button onClick={()=>{deleteComent(c.id)}} >삭제</button>
+                </li>
+            ))
+            }
+        </ul>
     );
 };
 
