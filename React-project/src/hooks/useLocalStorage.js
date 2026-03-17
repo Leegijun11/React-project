eba5f091b68dd72559416720811120ef57514179
Nostay <nst6830@gmail.com>
2026-03-17 17:39:12 +0900

    uselocalstorage 둘다살림

--- React-project/src/hooks/useLocalStorage.js
+++ React-project/src/hooks/useLocalStorage.js
@@ -1,10 +1,14 @@
-<<<<<<< HEAD
 import { useState, useEffect } from "react";
 
 export default function useLocalStorage(key, initialValue) {
   const [storedValue, setStoredValue] = useState(() => {
-    const item = localStorage.getItem(key);
-    return item ? JSON.parse(item) : initialValue;
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      localStorage.removeItem(key);
+      return initialValue;
+    }
   });
 
   useEffect(() => {
@@ -12,22 +16,4 @@ export default function useLocalStorage(key, initialValue) {
   }, [key, storedValue]);
 
   return [storedValue, setStoredValue];
-=======
-import { useState } from "react";
-
-export const useLocalStorage_list = (obj) => {
-  const [value, setValue] = useState(()=>{
-    try {
-      const obj2 = localStorage.getItem(obj)
-      return obj2 ? JSON.parse(obj2) : [];
-  
-    }
-    catch (error){
-      localStorage.removeItem(obj);
-      return []
-    }
-
-  })
-  return [value,setValue];
->>>>>>> 2245615512e4894f70026982b816c7db67e3a343
 }
